83991d4abf332c3230b827dab7b262cc552f9098
Axat Sachani <98816145+AxatSachani@users.noreply.github.com>
2023-08-09 21:09:17 +0530

    Time: 119 ms (90.91%), Space: 55.3 MB (18.18%) - LeetHub

---
+++ 2720-minimize-the-maximum-difference-of-pairs/2720-minimize-the-maximum-difference-of-pairs.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} nums
+ * @param {number} p
+ * @return {number}
+ */
+var minimizeMax = function (nums, p) {
+    nums.sort((a, b) => a - b);
+
+    const isContainEnough = ({ diff }) => {
+        let c = 0;
+        let i = 0;
+        while (i < nums.length - 1 && c < p) {
+            if (nums[i + 1] - nums[i] <= diff) {
+                c++;
+                i += 2;
+            } else {
+                i++;
+            }
+        }
+        return c >= p;
+    }
+
+    let left = 0;
+    let right = nums[nums.length - 1] - nums[0];
+
+    while (left < right) {
+        let mid = Math.floor((left + right) / 2);
+        if (isContainEnough({ diff: mid })) {
+            right = mid;
+        } else {
+            left = mid + 1;
+        }
+    }
+
+    return left;
+};

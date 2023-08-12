2fc5257a2fb61d3704418285daf69d84ef1be779
Axat Sachani <98816145+AxatSachani@users.noreply.github.com>
2023-08-12 22:50:20 +0530

    Time: 69 ms (71.31%), Space: 44.9 MB (44.46%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var myAtoi = function (s) {
+    s = s.trim()
+    if (!(/^[-+\d]/.test(s))) return 0
+    let i = 0, sign = 1, result = 0
+    if (s[i] == '+') { sign = 1; i++ }
+    else if (s[i] == '-') { sign = -1; i++ }
+    while (i < s.length && /^\d$/.test(s[i])) {
+        result = result * 10 + parseInt(s[i])
+        i++
+    }
+    result = result * sign
+    result = Math.max(Math.min(result, Math.pow(2, 31) - 1), -Math.pow(2, 31));  // Check for integer overflow
+    return result;
+};
+
+
+
+

3a21040ce241f71681f996a5f810582d3273167f
Axat Sachani <98816145+AxatSachani@users.noreply.github.com>
2023-08-13 10:11:52 +0530

    Time: 86 ms (49.06%), Space: 42.3 MB (72.06%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,17 +4,13 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
-    const index = []
     for (let i = 0; i < nums.length; i++) {
         let j = i + 1
         while (j < nums.length) {
             if (nums[i] + nums[j] == target) {
-                index.push(i)
-                index.push(j)
-                break
+                return [i, j]
             }
             j++
         }
     }
-    return index
 };

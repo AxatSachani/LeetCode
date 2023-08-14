767a97359a17758c57d3e3f31b49ec348db8dcbf
Axat Sachani <98816145+AxatSachani@users.noreply.github.com>
2023-08-14 22:55:43 +0530

    Time: 53 ms (86.14%), Space: 44.7 MB (27.4%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.js
+++ 0414-third-maximum-number/0414-third-maximum-number.js
@@ -3,6 +3,8 @@
  * @return {number}
  */
 var thirdMax = function (nums) {
-    const result = Array.from(new Set(nums.sort((a, b) => b - a)))
-    return result.length >= 3 ? result[2] : result[0]
+    nums = nums.sort((a, b) => b - a)
+    nums = new Set(nums)
+    nums = Array.from(nums)
+    return nums.length >= 3 ? nums[2] : nums[0]
 };

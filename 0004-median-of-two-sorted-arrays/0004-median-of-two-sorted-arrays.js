d211401e1486af33dec50a9880937ffe985e28d7
Axat Sachani <98816145+AxatSachani@users.noreply.github.com>
2023-08-20 23:40:38 +0530

    Time: 78 ms (96.75%), Space: 48 MB (51.62%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.js
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.js
@@ -6,12 +6,11 @@
 var findMedianSortedArrays = function (nums1, nums2) {
     nums1 = nums1.concat(nums2).sort((a, b) => a - b)
     if (nums1.length == 1) return nums1[0]
-    const mid = Math.floor(nums1.length / 2)
-    if (nums1.length % 2 == 0) {
+    else if (nums1.length % 2 == 0) {
         const middle1 = Math.floor(nums1.length / 2);
         const middle2 = middle1 - 1;
         return (nums1[middle1] + nums1[middle2]) / 2
     }
-    else return nums1[mid]
+    else return nums1[Math.floor(nums1.length / 2)]
 
 };

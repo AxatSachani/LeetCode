8c479e487701b25a115b8673b03d84631c46c81d
Axat Sachani <98816145+AxatSachani@users.noreply.github.com>
2023-08-25 23:27:34 +0530

    Time: 64 ms (69.35%), Space: 43.5 MB (87.37%) - LeetHub

---
+++ 0097-interleaving-string/0097-interleaving-string.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @param {string} s3
+ * @return {boolean}
+ */
+var isInterleave = function (s1, s2, s3) {
+    if (s1.length + s2.length !== s3.length) return false;
+
+    const dp = new Array(s2.length + 1).fill(false);
+
+    for (let i = 0; i <= s1.length; i++) {
+        for (let j = 0; j <= s2.length; j++) {
+            if (i === 0 && j === 0) {
+                dp[j] = true;
+            } else if (i === 0) {
+                dp[j] = dp[j - 1] && s2[j - 1] === s3[i + j - 1];
+            } else if (j === 0) {
+                dp[j] = dp[j] && s1[i - 1] === s3[i + j - 1];
+            } else {
+                dp[j] = (dp[j] && s1[i - 1] === s3[i + j - 1]) || (dp[j - 1] && s2[j - 1] === s3[i + j - 1]);
+            }
+        }
+    }
+
+    return dp[s2.length];
+};
+

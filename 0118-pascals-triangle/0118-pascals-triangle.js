17020b9c4b29737901ecbfc1454acf8835743532
Axat Sachani <98816145+AxatSachani@users.noreply.github.com>
2023-09-08 22:48:24 +0530

    Time: 51 ms (74.7%), Space: 42.3 MB (39.59%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.js
+++ 0118-pascals-triangle/0118-pascals-triangle.js
@@ -3,14 +3,14 @@
  * @return {number[][]}
  */
 var generate = function (numRows) {
-    const triangle = []
+    let triangle = [], tmp = []
     for (let i = 1; i <= numRows; i++) {
-        const tmp = []
         for (let j = 0; j < i; j++) {
             if (j == 0 || j == i - 1) tmp[j] = 1
             else tmp[j] = triangle[i - 2][j - 1] + triangle[i - 2][j]
         }
         triangle[i - 1] = tmp
+        tmp = []
     }
     return triangle
 };

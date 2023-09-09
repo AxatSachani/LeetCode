9be50434d21bf5e9fd0a998318bccc1b093df5ee
Axat Sachani <98816145+AxatSachani@users.noreply.github.com>
2023-09-09 11:58:12 +0530

    Time: 2161 ms (57.18%), Space: 0B (100%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
-Select e.name as name, COALESCE( b.bonus, null) AS bonus 
+Select e.name as name, b.bonus AS bonus 
 from Employee e left join Bonus b on e.empId = b.empId where b.bonus < 1000 OR Bonus IS NULL ;

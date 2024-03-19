ff0bb1cc9b551466a6e33780edde6e846498c189
Axat Sachani <98816145+AxatSachani@users.noreply.github.com>
2024-03-19 22:29:34 +0530

    Time: 57 ms (26.31%), Space: 49.1 MB (7.51%) - LeetHub

---
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function (val) {
+    return {
+        toBe: (testCase) => {
+            if (val === testCase) return true
+            else throw new Error("Not Equal")
+        },
+        notToBe: (testCase) => {
+            if (val === testCase) throw new Error("Equal")
+            else return true
+        }
+    }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */

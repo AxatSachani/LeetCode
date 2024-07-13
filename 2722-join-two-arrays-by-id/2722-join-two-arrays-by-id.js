627a159b5e771e22946ba977325da1619d874b4f
Axat Sachani <98816145+AxatSachani@users.noreply.github.com>
2024-07-14 01:06:04 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2722-join-two-arrays-by-id/2722-join-two-arrays-by-id.js
@@ -0,0 +1,22 @@
+/**
+ * @param {Array} arr1
+ * @param {Array} arr2
+ * @return {Array}
+ */
+var join = function (arr1, arr2) {
+    const map = new Map();
+
+    arr1.forEach(item => {
+        map.set(item.id, item);
+    });
+
+    arr2.forEach(item => {
+        const value = map.get(item.id);
+        console.log(value)
+        if (value) map.set(item.id, { ...value, ...item });
+        else map.set(item.id, item);
+    });
+
+    // Convert map values back to array and return
+    return Array.from(map.values());
+};

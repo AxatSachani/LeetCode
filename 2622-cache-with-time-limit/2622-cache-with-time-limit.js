acda42c8aa6835173ab61ed865beecd77ca02016
Axat Sachani <98816145+AxatSachani@users.noreply.github.com>
2024-07-13 23:57:49 +0530

    Time: 49 ms (90.55%), Space: 49.1 MB (18.93%) - LeetHub

--- 2622-cache-with-time-limit/2622-cache-with-time-limit.js
+++ 2622-cache-with-time-limit/2622-cache-with-time-limit.js
@@ -46,3 +46,64 @@ TimeLimitedCache.prototype.count = function() {
 TimeLimitedCache.prototype.remove = function(key) {
   delete this.cache[key];
 };
+
+
+
+// var TimeLimitedCache = function () {
+//     this.cache = new Map();
+// };
+
+// /** 
+//  * @param {number} key
+//  * @param {number} value
+//  * @param {number} duration time until expiration in ms
+//  * @return {boolean} if un-expired key already existed
+//  */
+// TimeLimitedCache.prototype.set = function (key, value, duration) {
+//     // Clear existing timer if key already exists
+//     if (this.cache.has(key)) return false
+
+//     // Set new value and schedule expiration
+//     const timer = setTimeout(() => {
+//         this.remove(key);
+//     }, duration);
+
+//     this.cache.set(key, {
+//         value: value,
+//         timer: timer
+//     });
+//     // Return true if the key was updated, false if it was newly added
+//     return this.get(key);
+// }
+// /** 
+//  * @param {number} key
+//  * @return {number} value associated with key
+//  */
+// TimeLimitedCache.prototype.get = function (key) {
+//     if (this.cache.has(key)) {
+//         return this.cache.get(key).value;
+//     } else {
+//         return -1; // Or any other default value indicating key not found
+//     }
+// };
+
+// /** 
+//  * @return {number} count of non-expired keys
+//  */
+// TimeLimitedCache.prototype.count = function () {
+//     return this.cache.size;
+// };
+
+// TimeLimitedCache.prototype.remove = function (key) {
+//     if (this.cache.has(key)) {
+//         clearTimeout(this.cache.get(key).timer);
+//         this.cache.delete(key);
+//     }
+// };
+
+// /**
+//  * const timeLimitedCache = new TimeLimitedCache()
+//  * timeLimitedCache.set(1, 42, 1000); // false
+//  * timeLimitedCache.get(1) // 42
+//  * timeLimitedCache.count() // 1
+//  */

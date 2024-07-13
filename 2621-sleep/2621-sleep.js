39b2dad948d5e92437147ea220c98401bc8ffa3b
Axat Sachani <98816145+AxatSachani@users.noreply.github.com>
2024-07-13 22:56:19 +0530

    Time: 51 ms (65.76%), Space: 48.8 MB (52.07%) - LeetHub

--- 2621-sleep/2621-sleep.js
+++ 2621-sleep/2621-sleep.js
@@ -3,9 +3,7 @@
  * @return {Promise}
  */
 async function sleep(millis) {
-    return new Promise((resolve) => {
-        setTimeout(resolve, millis);
-    })
+    return new Promise(resolve => setTimeout(resolve, millis));
 }
 
 /** 

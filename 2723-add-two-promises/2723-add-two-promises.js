bf7142d012a0dc7078f9333caafa346f275ee635
Axat Sachani <98816145+AxatSachani@users.noreply.github.com>
2024-07-13 22:42:22 +0530

    Time: 49 ms (98.3%), Space: 49.7 MB (31.27%) - LeetHub

--- 2723-add-two-promises/2723-add-two-promises.js
+++ 2723-add-two-promises/2723-add-two-promises.js
@@ -3,10 +3,15 @@
  * @param {Promise} promise2
  * @return {Promise}
  */
-var addTwoPromises = async function (promise1, promise2) {
-    const [val1, val2] = await Promise.all([promise1, promise2])
-    return val1 + val2
+const addTwoPromises = async (promise1, promise2) => {
+    try {
+        const [val1, val2] = await Promise.all([promise1, promise2]);
+        return val1 + val2;
+    } catch (error) {
+        throw error; // Propagate any errors from the promises
+    }
 };
+
 
 /**
  * addTwoPromises(Promise.resolve(2), Promise.resolve(2))

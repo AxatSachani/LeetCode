8be936c643a8ceaa9dd7a8f94e2ad12dc7c2d85a
Axat Sachani <98816145+AxatSachani@users.noreply.github.com>
2024-07-13 21:37:34 +0530

    Time: 72 ms (17.06%), Space: 49.8 MB (77.06%) - LeetHub

--- 2629-function-composition/2629-function-composition.js
+++ 2629-function-composition/2629-function-composition.js
@@ -5,11 +5,8 @@
 var compose = function (functions) {
 
     return function (x) {
-        for (let i = functions.length - 1; i >= 0; i--) {
-            x = functions[i](x);
-        }
-        return x;
-    };
+        return functions.reduceRight((acc, fn) => fn(acc), x);
+    }
 };
 
 /**

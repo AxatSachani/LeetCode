4f69b388e2ab6e7253bafdc6983d5985114b617c
Axat Sachani <98816145+AxatSachani@users.noreply.github.com>
2024-07-14 01:07:44 +0530

    Time: 330 ms (62.06%), Space: 92.4 MB (66.27%) - LeetHub

--- 2722-join-two-arrays-by-id/2722-join-two-arrays-by-id.js
+++ 2722-join-two-arrays-by-id/2722-join-two-arrays-by-id.js
@@ -6,17 +6,23 @@
 var join = function (arr1, arr2) {
     const map = new Map();
 
+    // Populate map with arr1 items
     arr1.forEach(item => {
         map.set(item.id, item);
     });
 
+    // Merge arr2 items into map
     arr2.forEach(item => {
-        const value = map.get(item.id);
-        console.log(value)
-        if (value) map.set(item.id, { ...value, ...item });
-        else map.set(item.id, item);
+        // Check if item exists in map
+        if (map.has(item.id)) {
+            // Merge properties into existing item in map
+            Object.assign(map.get(item.id), item);
+        } else {
+            // Add new item to map
+            map.set(item.id, item);
+        }
     });
 
-    // Convert map values back to array and return
+    // Convert map values back to array and sort by id
     return Array.from(map.values()).sort((a, b) => a.id - b.id);
 };

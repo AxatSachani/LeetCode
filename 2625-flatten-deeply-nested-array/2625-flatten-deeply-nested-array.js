e0eb3b109a60cff0c9c20b7f58f6b1897b01be80
Axat Sachani <98816145+AxatSachani@users.noreply.github.com>
2024-07-14 01:19:42 +0530

    Time: 153 ms (47.09%), Space: 79.4 MB (30.18%) - LeetHub

---
+++ 2625-flatten-deeply-nested-array/2625-flatten-deeply-nested-array.js
@@ -0,0 +1,17 @@
+/**
+ * @param {Array} arr
+ * @param {number} depth
+ * @return {Array}
+ */
+var flat = function (arr, n) {
+    if (n == 0) return arr;
+    var ans = [];
+    for (var i = 0; i < arr.length; i++) {
+        if (Array.isArray(arr[i])) {
+            ans.push(...flat(arr[i], n - 1));
+        } else {
+            ans.push(arr[i]);
+        }
+    }
+    return ans;
+};

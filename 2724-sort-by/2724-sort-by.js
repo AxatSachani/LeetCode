c18d5f4027445c1244545ab844869845205a999c
Axat Sachani <98816145+AxatSachani@users.noreply.github.com>
2024-07-14 00:48:57 +0530

    Time: 115 ms (93.48%), Space: 64.8 MB (83.24%) - LeetHub

--- 2724-sort-by/2724-sort-by.js
+++ 2724-sort-by/2724-sort-by.js
@@ -4,13 +4,5 @@
  * @return {Array}
  */
 var sortBy = function (arr, fn) {
-    const n = arr.length;
-    for (let i = 0; i < n - 1; i++) {
-        for (let j = 0; j < n - 1 - i; j++) {
-            if (fn(arr[j]) > fn(arr[j + 1])) {
-                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
-            }
-        }
-    }
-    return arr;
+    return arr.sort((a, b) => fn(a) - fn(b))
 };

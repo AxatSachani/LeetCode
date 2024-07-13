70a54d2112fe89849c4e443810d6a51aee1e8b6a
Axat Sachani <98816145+AxatSachani@users.noreply.github.com>
2024-07-13 19:26:47 +0530

    Time: 58 ms (64.13%), Space: 51.5 MB (71.87%) - LeetHub

--- 2665-counter-ii/2665-counter-ii.js
+++ 2665-counter-ii/2665-counter-ii.js
@@ -6,7 +6,7 @@ var createCounter = function (init) {
     let current = init
     return {
         increment: () => {
-            return ++current
+            return (++current)
         },
         reset: () => {
             return (current = init)
